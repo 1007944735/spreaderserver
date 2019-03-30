616339cdb302180f745f217dd61a15efd9b43708
1007944735 <1007944735@qq.com>
2019-03-30 14:48:33 +0800

    111

--- src/main/java/com/sgevf/spreaderserver/controller/S0007.java
+++ src/main/java/com/sgevf/spreaderserver/controller/S0007.java
@@ -5,9 +5,12 @@ import com.sgevf.spreaderserver.entity.response.Response;
 import com.sgevf.spreaderserver.service.PubService;
 import com.sgevf.spreaderserver.utils.HttpResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.support.SimpleTriggerContext;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class S0007 {
@@ -15,16 +18,19 @@ public class S0007 {
     private PubService pubService;
 
     private List<RedPacketSearchDto> results;
+    private Map<String,List<RedPacketSearchDto>> map;
 
     @ResponseBody
     @RequestMapping(value = "/S0007", method = RequestMethod.POST)
-    public Response<List<RedPacketSearchDto>> s0007(@RequestParam("longitude") String longitude, @RequestParam("latitude") String latitude, @RequestParam("type") String type) {
+    public Response<Map<String,List<RedPacketSearchDto>>> s0007(@RequestParam("longitude") String longitude, @RequestParam("latitude") String latitude, @RequestParam("type") String type) {
+        map=new HashMap<>();
         try {
             results = pubService.searchByOrder(longitude, latitude, type);
+            map.put("list",results);
         } catch (Exception e) {
             e.printStackTrace();
             return new Response<>(HttpResponse.ERROR, "系统错误", null);
         }
-        return new Response<>(HttpResponse.SUCCESS, "成功", results);
+        return new Response<>(HttpResponse.SUCCESS, "成功", map);
     }
 }
--- src/main/java/com/sgevf/spreaderserver/controller/S0008.java
+++ src/main/java/com/sgevf/spreaderserver/controller/S0008.java
@@ -7,19 +7,25 @@ import com.sgevf.spreaderserver.utils.HttpResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class S0008 {
     @Autowired
     private PubService pubService;
 
+    private Map<String,List<RedPacketSearchDto>> map;
+
     @ResponseBody
     @RequestMapping(value = "/S0008", method = RequestMethod.POST)
-    public Response<List<RedPacketSearchDto>> s0008(@RequestParam("longitude") String longitude, @RequestParam("latitude") String latitude, @RequestParam(value = "type", required = false) String type, @RequestParam(value = "number", required = false) String number, @RequestParam(value = "amount", required = false) String amount) {
+    public Response< Map<String,List<RedPacketSearchDto>>> s0008(@RequestParam("longitude") String longitude, @RequestParam("latitude") String latitude, @RequestParam(value = "type", required = false) String type, @RequestParam(value = "number", required = false) String number, @RequestParam(value = "amount", required = false) String amount) {
+        map=new HashMap<>();
         String[] numbers = number != null ? number.split(",") : null;
         String[] amounts = amount != null ? amount.split(",") : null;
         List<RedPacketSearchDto> result = pubService.searchByFilter(longitude, latitude, type, numbers, amounts);
-        return new Response<>(HttpResponse.SUCCESS, "成功", result);
+        map.put("list",result);
+        return new Response<>(HttpResponse.SUCCESS, "成功", map);
     }
 }
