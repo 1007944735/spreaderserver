b32c6ca5ffc8ab47f89d146d95f2703f78c0b520
王嘉杰 <wangjiajie@zhuojianchina.com>
2019-05-07 21:33:17 +0800

    11111

--- src/main/java/com/sgevf/spreaderserver/Md5Test.java
+++ src/main/java/com/sgevf/spreaderserver/Md5Test.java
@@ -3,6 +3,8 @@ package com.sgevf.spreaderserver;
 import com.sgevf.spreaderserver.utils.DateUtils;
 import sun.awt.geom.AreaOp;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Random;
@@ -21,9 +23,11 @@ public class Md5Test {
 //                double maxNumber = all - 0.01 * (num - 1 - i);
                 double maxNumber = all/(num-i)*2;
                 double actual = (maxNumber - 0.01) * new Random().nextDouble() + 0.01;
-                all = all - actual;
-                money[i]=actual;
-                System.out.println("第" + i + "个：" + actual);
+                BigDecimal bd=new BigDecimal(Double.toString(actual)).setScale(2, RoundingMode.HALF_UP);
+                all = all -bd.doubleValue() ;
+//                System.out.println("第" + i + "all个：" + all);
+                money[i]=bd.doubleValue();
+                System.out.println("第" + i + "个：" + money[i]);
             }
         }
         for(int i=0;i<money.length;i++){
